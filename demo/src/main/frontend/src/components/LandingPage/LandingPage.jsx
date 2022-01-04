d71d943ff730554dafa8fd2efb28078cfd59922a
Mateo Paluci <81190495+mateo0697@users.noreply.github.com>
2022-01-04 11:26:42 -0300

    Update LandingPage.jsx

--- demo/src/main/frontend/src/components/LandingPage/LandingPage.jsx
+++ demo/src/main/frontend/src/components/LandingPage/LandingPage.jsx
@@ -10,10 +10,6 @@ function LandingPage() {
          Book Search
       </Link>
         <img alt="Fotos de perritos" style={{width: "100%" ,height:"100%"}} src="https://fondosmil.com/fondo/1674.jpg"/>
-{/*    <div style={{position:"relative", display:"flex", alingItems:"center"}}>
-      <div style={{position:"absolute", top:"10%", left:"10%", textDecoration: 'none', color:"black", fontSize:"50px"}}>Averigua mas sobre tu perro favorito <Link style={{textDecoration: 'underline', color:"black", fontSize:"50px"}} to="/home">aqui</Link></div>
-      <img alt="Fotos de perritos" style={{width: "100%" ,height:"100%"}} src="https://www.happets.com/blog/wp-content/uploads/2020/01/las-razas-de-perros-mas-obedientes.jpg"/>
-    </div>*/}
     </div>
   );
 }
